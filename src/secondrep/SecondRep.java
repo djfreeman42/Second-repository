b271d68a0ce9f5531bd40020623185641514045d
df23@students.ptcollege.edu <>
2018-10-22 15:46:40 -0400

    First commit 2nd

---
+++ src/secondrep/SecondRep.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package secondrep;
+
+/**
+ *
+ * @author df23
+ */
+public class SecondRep {
+
+    /**
+     * @param args the command line arguments
+     */
+     public static void main(String[] args) {
+        Freeman();
+    }
+    public static void Freeman()
+    {
+        System.out.println("Dylan Freeman2");
+    }
+    
+}
